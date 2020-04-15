ed7ab76a25603ff3ce42a5709023d28b0364116c
EKyero <a249153119@gmail.com>
2020-04-15 17:19:41 +0800

    修改argon

--- diy.sh
+++ diy.sh
@@ -5,8 +5,8 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=================================================
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#echo '修改网关地址'
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 cd package
 mkdir openwrt-packages
 cd openwrt-packages
@@ -14,3 +14,8 @@ git clone https://github.com/tzxiaozhen88/koolproxyR.git
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
 git clone https://github.com/Lienol/openwrt-package.git
 git clone https://github.com/vernesong/OpenClash.git
+rm -rf luci-theme-argon 
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+
+echo '修改默认主题'
+sed -i 's/config internal themes/config internal themes\n    option Argon  \"\/luci-static\/argon\"/g' feeds/luci/modules/luci-base/root/etc/config/luci
