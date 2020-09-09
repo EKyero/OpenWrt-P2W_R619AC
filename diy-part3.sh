c4929ebfd2c475ae821076e0a99db1368c352a7a
EKyero <a249153119@ gamil.com>
2020-09-10 00:28:49 +0800

    add CTCGFW

---
+++ diy-part3.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#=============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=============================================================
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+cd package
+mkdir openwrt-packages
+cd openwrt-packages
+git clone https://github.com/tzxiaozhen88/koolproxyR.git
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
+git clone https://github.com/Lienol/openwrt-package.git
+git clone -b master https://github.com/vernesong/OpenClash.git
+git clone https://github.com/jerrykuku/lua-maxminddb.git 
+git clone https://github.com/jerrykuku/luci-app-vssr.git
+cd .. 
+cd lean  
+rm -rf luci-theme-argon 
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
