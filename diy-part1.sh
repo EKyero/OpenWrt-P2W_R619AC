106109daad82cc8aa4bc8ba0f9e82ed41310aac1
EKyero <a249153119@ gamil.com>
2020-10-16 01:39:21 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,12 +16,9 @@
 cd package
 mkdir openwrt-packages
 cd openwrt-packages
-git clone https://github.com/tzxiaozhen88/koolproxyR.git
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
 git clone https://github.com/Lienol/openwrt-package.git
 git clone -b master https://github.com/vernesong/OpenClash.git
-git clone https://github.com/jerrykuku/lua-maxminddb.git 
-git clone https://github.com/jerrykuku/luci-app-vssr.git
 cd .. 
 cd lean  
 rm -rf luci-theme-argon 
